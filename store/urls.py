7c0ae9f059a67856a6e82e735d70b98c8d67db4e
Alexander Musonda <musondaalexander97@gmail.com>
2023-02-15 23:02:09 +0200

    updated the file structure
    made changes across the entire project

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,6 @@
-from django.urls import URLPattern, path
-from .import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.home, name= 'home'),
-    path('index',views.home_two, name = 'index'),
-     path('clothes',views.clothes, name = 'clothes'),
+    path('', views.home, name='home'),
 ]
--- store/views.py
+++ store/views.py
@@ -2,12 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request,'store/index.html')
-
-# for testing if the html file are loading 
-
-def home_two(request):
-    return render(request,'store/index-2.html')
-def clothes(request):
-    return render(request,'store/index-3.html')
+def home(reuest):
+    return render(reuest, 'index.html')
