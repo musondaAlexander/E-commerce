029f693ed81cf4aecf044226985da6d8afb0a541
Alexander Musonda <musondaalexander97@gmail.com>
2023-02-18 00:36:35 +0200

    Added all the Views and urls
    Added the Templates for the anchor pages of the site.

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-     path('organic', views.organic , name='organic')
+     path('organic', views.organic , name='organic'),
+     path('tech', views.tech, name='tech'),
+     path('beauty', views.beauty, name='beauty'),
+     
 ]
